802b6ec1c10fb09a636b574f378475451fa8f056
XalaAbo <76926045+XalaAbo@users.noreply.github.com>
2022-03-03 17:24:47 +0100

    Update shards.js

--- shards.js
+++ shards.js
@@ -1,1 +1,20 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const express = require("express");
+const app = express();
+const http = require(http);
+const scarew = new Discord.ShardingManager("./bot.js", {
 
+  totalShards: "auto",
+  token: "Bot Token" // Buraya botunuzun tokeninizi yazınız.
+});
+
+scarew.spawn();
+
+scarew.on("launch", shard => {
+  console.log(*${shard.id}* ID shard started.);
+});
+
+setTimeout(() => {
+  scarew.broadcastEval("process.exit()");
+}, 21600000);
